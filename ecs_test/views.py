afec552da8e4b716600f61c9d30ce5209a0922c8
John3210of <john3210of@gmail.com>
2024-11-27 10:15:48 +0900

    ecs cicd

--- ecs_test/views.py
+++ ecs_test/views.py
@@ -5,6 +5,6 @@ from rest_framework import status
 @api_view(['GET'])
 def index(request):
     data = {"result": "success",
-            "data": [{"id":"itstudy", "name":"군계"},
-                     {"id":"ggangpae1", "name":"아담"}]}
+            "data": [{"id":"john", "name":"한"},
+                     {"id":"hi", "name":"world"}]}
     return Response(data, status=status.HTTP_200_OK)
