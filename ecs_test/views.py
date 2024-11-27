6ac3f1876a6866a1f5a522b7509fbae4fd1b86b5
John3210of <john3210of@gmail.com>
2024-11-27 10:58:42 +0900

    check lb

--- ecs_test/views.py
+++ ecs_test/views.py
@@ -1,10 +1,14 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+import socket
 
 @api_view(['GET'])
 def index(request):
+    server_ip = socket.gethostbyname(socket.gethostname())
     data = {"result": "success",
             "data": [{"id":"john", "name":"한"},
-                     {"id":"hi", "name":"world"}]}
+                     {"id":"hi", "name":"world"}],
+            "server_ip": server_ip
+            }
     return Response(data, status=status.HTTP_200_OK)
