438f4ff2ec661478cc648bfaaa0e315ef09502a7
John3210of <john3210of@gmail.com>
2024-11-27 16:58:43 +0900

    show data as json

--- ecs_test/views.py
+++ ecs_test/views.py
@@ -1,14 +1,24 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+from django.db import connection
 import socket
 
 @api_view(['GET'])
 def index(request):
+    '''
+        model에 대해 정의 하지 않고, 조회만 시도합니다.
+        load balancer 정상 작동 검증을 위해 현재 서버의 ip를 같이 출력합니다.
+    '''
     server_ip = socket.gethostbyname(socket.gethostname())
-    data = {"result": "success",
-            "data": [{"id":"john", "name":"한"},
-                     {"id":"hi", "name":"world"}],
-            "server_ip": server_ip
-            }
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT id, name, age, gender, region FROM user")
+        columns = [col[0] for col in cursor.description]
+        users = [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+    data = {
+        "result": "success",
+        "data": users,
+        "server_ip": server_ip
+    }
     return Response(data, status=status.HTTP_200_OK)
